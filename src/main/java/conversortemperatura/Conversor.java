65231f86b701843afc67d04bc96870db46654b78
okita <109244075+okita-ok@users.noreply.github.com>
2023-10-13 22:37:23 -0400

    Commit com Erro no Conversor.java

--- src/main/java/conversortemperatura/Conversor.java
+++ src/main/java/conversortemperatura/Conversor.java
@@ -4,7 +4,7 @@ public class Conversor{
     public double celsiusFahrenheit(double celsius){
         double resultado = (celsius*1.8)+32;
         //System.out.printf("Temperatura em Fahrenheit: %.1f\n", resultado);
-        return resultado;
+        return resultado
     }
     public double fahrenheitCelsius(double fahrenheit){
         double resultado = (fahrenheit-32)/1.8;
